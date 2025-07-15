4bb58b5e0db80fbe5c2bf500cb02f53ee0ab6603
hlanhtetkyaw <hlan1559@gmail.com>
2025-07-16 01:58:27 +0630

    [UPDATE] index

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -10,15 +10,29 @@
     <ul>
         <li>Article </li>
         <ul>
-            <?php /*
-            <?php foreach($articles as $article):?>
-                <li><?php echo $article['title']?></li>
-            <?php endforeach?> normal php way
-            */ ?>
             @foreach ( $articles as $article )
                 <li>{{$article['title']}}</li>
             @endforeach
         </ul>
     </ul>
 </body>
-</html>
+</html> -->
+@extends('layouts.app')
+
+@section("content")
+ <div class="container">
+    @foreach ($articles as $article)
+        <div class="card mb-2">
+            <div class="card-body">
+                <h5 class="card-title">{{ $article->title }}</h5>
+                <div class="card-subtitle mb-2 text-muted small">
+                    {{ $article->created_at->diffForHumans() }}
+                </div>
+                <p class="card-text">{{ $article->body }}</p>
+                <a href="{{ url("/articles/detail/$article->id") }}"></a>
+                View Detail &raquo;
+            </div>
+        </div>
+    @endforeach
+ </div>
+@endsection
