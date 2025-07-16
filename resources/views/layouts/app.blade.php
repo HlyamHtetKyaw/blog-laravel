f02f52094b53893dc0da516b96ea73f8193dff18
hlanhtetkyaw <hlan1559@gmail.com>
2025-07-16 23:33:13 +0630

    [UPDATE] app name

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,11 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
-
+                        <li class="nav-item">
+                            <a class="nav-link text-success" href="{{ url('/articles/add') }}">
+                                +Add Articles
+                            </a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
