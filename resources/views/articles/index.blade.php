20a22a59498ab705ae62da94695ed989a746c666
hlanhtetkyaw <hlan1559@gmail.com>
2025-07-16 23:18:57 +0630

    [ADD] Article detail

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -21,6 +21,7 @@
 
 @section("content")
  <div class="container">
+    {{ $articles->links() }}
     @foreach ($articles as $article)
         <div class="card mb-2">
             <div class="card-body">
@@ -29,8 +30,7 @@
                     {{ $article->created_at->diffForHumans() }}
                 </div>
                 <p class="card-text">{{ $article->body }}</p>
-                <a href="{{ url("/articles/detail/$article->id") }}"></a>
-                View Detail &raquo;
+                <a href="{{ url("/articles/details/$article->id") }}">View Detail &raquo;</a>
             </div>
         </div>
     @endforeach
